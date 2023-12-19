c6e6db6d4211130c43fb5623ead747321be0b6b9
haru_ban <h.yaegashi.sys23@morijyobi.ac.jp>
2023-12-19 13:21:32 +0900

    javaのプログラムを変更

--- Main.java
+++ Main.java
@@ -1,13 +1,15 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("身長を入力してください");
-        double height = sc.nextDouble();
-        System.out.println("体重を入力してください");
-        double weight = sc.nextDouble();
-        double bmi = (weight / (height*height));
-        System.out.println("BMI値は" + bmi + "です");
+        for(int i = 1; i <= 2; i++) {
+            System.out.println("身長を入力してください");
+            double height = sc.nextDouble();
+            System.out.println("体重を入力してください");
+            double weight = sc.nextDouble();
+            double bmi = weight / ((height / 100) * (height / 100));
+            System.out.println("BMI値は" + bmi + "です");
+        }
     }
 }
